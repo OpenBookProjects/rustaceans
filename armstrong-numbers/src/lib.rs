fb23a0581d2bd6990d906efcf0062aed6c9a2934
ZoomQuiet <zoomquiet+git@gmail.com>
2022-10-21 20:58:58 +0800

    📖 DOC: exerciam:rust::9;10/103.. as py

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    //unimplemented!("true if {} is an armstrong number", num)
+    println!("{}'s len:{}", num, num.to_string().len());
+    let p = num.to_string().len() as u32;
+    println!("pow base {}", p);
+    for n in num.to_string().chars() {
+        println!("one by one {}", n);
+        println!(
+            "{} pow {} got {}",
+            n.to_digit(10).unwrap_or(0),
+            p,
+            n.to_digit(10).unwrap_or(0).pow(p)
+        );
+    }
+
+    true
 }
---
+++ armstrong-numbers/src/main.rs
@@ -0,0 +1,6 @@
+use armstrong_numbers::*;
+
+fn main() {
+    let a = 12;
+    println!("{} is? {}", a, is_armstrong_number(a));
+}
